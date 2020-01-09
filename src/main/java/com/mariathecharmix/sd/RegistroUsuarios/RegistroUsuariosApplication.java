efde797cf6263d3c2ae34b4796a106ad5a5b1a23
MariaPaulaS <mariathecharmix@hotmail.com>
2020-01-08 23:41:23 -0500

    Configuracion de STS y conexion a la base de datos

---
+++ src/main/java/com/mariathecharmix/sd/RegistroUsuarios/RegistroUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.mariathecharmix.sd.RegistroUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariathecharmix/sd/RegistroUsuarios/RegistroUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariathecharmix.sd.RegistroUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
