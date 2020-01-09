58f0036e2a841f403d266186c3e5aaa4e81330c1
MariaPaulaS <mariathecharmix@hotmail.com>
2020-01-09 11:04:43 -0500

    Creacion de entidades y migracion de tablas con Hibernate. Close #2

--- src/main/java/com/mariathecharmix/sd/RegistroUsuarios/RegistroUsuariosApplication.java
+++ src/main/java/com/mariathecharmix/sd/RegistroUsuarios/RegistroUsuariosApplication.java
@@ -2,8 +2,12 @@ package com.mariathecharmix.sd.RegistroUsuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.mariathecharmix.sd"})
 public class RegistroUsuariosApplication {
 
 	public static void main(String[] args) {
