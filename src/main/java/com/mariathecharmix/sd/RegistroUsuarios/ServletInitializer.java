b89be3ae51c90179a971bd703a47f0fa3e23907b
MariaPaulaS <mariathecharmix@hotmail.com>
2020-01-13 22:58:55 -0500

    Vistas HTML y controlador creados. Close #3

--- src/main/java/com/mariathecharmix/sd/RegistroUsuarios/ServletInitializer.java
+++ src/main/java/com/mariathecharmix/sd/RegistroUsuarios/ServletInitializer.java
@@ -3,6 +3,8 @@ package com.mariathecharmix.sd.RegistroUsuarios;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.mariathecharmix.sd.controller.RegistroUsuariosApplication;
+
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
